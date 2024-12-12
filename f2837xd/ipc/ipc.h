62a0a7a29d47a0fb45ec73c8a61b47f694b61b68
Oleg Aushev <aushevom@gmail.com>
2024-12-12 17:55:20 +0300

    add new ipc::Flag impl

--- f2837xd/ipc/ipc.h
+++ f2837xd/ipc/ipc.h
@@ -19,29 +19,65 @@ SCOPED_ENUM_DECLARE_BEGIN(Cpu) {
 const Cpu this_cpu = Cpu::cpu1;
 #elif defined(CPU2)
 const Cpu this_cpu = Cpu::cpu2;
+#else
+#error "CPU is not defined."
 #endif
 
-namespace newimpl {
+SCOPED_ENUM_DECLARE_BEGIN(FlagType) {
+    local,
+    remote,
+} SCOPED_ENUM_DECLARE_END(FlagType)
 
-class Flag {
+
+class NewFlag {
 private:
     uint32_t bitmask_;
     Cpu master_cpu_;
+    FlagType type_;
 public:
-    Flag(uint32_t flag_id, Cpu master_cpu)
+    NewFlag(uint32_t flag_id, Cpu master_cpu)
             : bitmask_(1 << flag_id), master_cpu_(master_cpu) {
         assert(flag_id < 32);
+        if (this_cpu == master_cpu_) {
+            type_ = FlagType::local;
+        } else {
+            type_ = FlagType::remote;
+        }
     }
+
+    FlagType type() const { return type_; }
+    bool is_local() const { return type_ == FlagType::local; }
+    bool is_remote() const { return type_ == FlagType::remote; }
 
     void set() {
-        assert(this_cpu == master_cpu_);
+        assert(is_local());
+        if (is_local()) {
+            IPCLtoRFlagSet(bitmask_);
+        }
+    }
+
+    void clear() {
+        if (is_local()) {
+            IPCLtoRFlagClear(bitmask_);
+        } else {
+            IPCRtoLFlagAcknowledge(bitmask_);
+        }
+    }
+
+    bool is_set() const {
+        if (is_local()) {
+            return IPCLtoRFlagBusy(bitmask_);
+        } else {
+            return IPCRtoLFlagBusy(bitmask_);
+        }
+    }
+
+    void wait_until_set() const {
+        while (!is_set()) {
+            // wait
+        }
     }
 };
-
-
-} // namespace newimpl
-
-
 
 namespace traits {
 struct singlecore{};
