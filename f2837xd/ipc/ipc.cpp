5f868e6c2fe4e12859e4e9657791d114c8230278
Oleg Aushev <aushevom@gmail.com>
2024-12-13 19:35:44 +0300

    remove obsolete ipc flag impl

--- f2837xd/ipc/ipc.cpp
+++ f2837xd/ipc/ipc.cpp
@@ -1,38 +1,15 @@
 #ifdef MCUDRV_C28X
 
-
 #include <mcudrv/c28x/f2837xd/ipc/ipc.h>
 
-
 namespace mcu {
-
 namespace c28x {
-
 namespace ipc {
 
 
-namespace flags {
-
-
-//#ifdef DUALCORE
-//const mcu::c28x::ipc::Mode mode = mcu::c28x::ipc::Mode::singlecore;
-//#else
-//const mcu::c28x::ipc::Mode mode = mcu::c28x::ipc::Mode::dualcore;
-//#endif
-//
-//mcu::c28x::ipc::Flag cpu1_periphery_configured(31, mode);
-//mcu::c28x::ipc::Flag cpu2_booted(30, mode);
-//mcu::c28x::ipc::Flag cpu2_periphery_configured(29, mode);
-
-
-} // namespace flags
-
 
 } // namespace ipc
-
 } // namespace c28x
-
 } // namespace mcu
-
 
 #endif
--- f2837xd/ipc/ipc.h
+++ f2837xd/ipc/ipc.h
@@ -29,13 +29,13 @@ SCOPED_ENUM_DECLARE_BEGIN(FlagType) {
 } SCOPED_ENUM_DECLARE_END(FlagType)
 
 
-class NewFlag {
+class Flag {
 private:
     uint32_t bitmask_;
     Cpu master_cpu_;
     FlagType type_;
 public:
-    NewFlag(uint32_t flag_id, Cpu master_cpu)
+    Flag(uint32_t flag_id, Cpu master_cpu)
             : bitmask_(1 << flag_id), master_cpu_(master_cpu) {
         assert(flag_id < 32);
         if (this_cpu == master_cpu_) {
@@ -79,115 +79,6 @@ public:
     }
 };
 
-namespace traits {
-struct singlecore{};
-struct dualcore{};
-struct primary{};
-struct secondary{};
-}
-
-
-SCOPED_ENUM_DECLARE_BEGIN(Mode) {
-    singlecore,
-    dualcore
-} SCOPED_ENUM_DECLARE_END(Mode)
-
-
-SCOPED_ENUM_DECLARE_BEGIN(Role) {
-    primary,
-    secondary
-} SCOPED_ENUM_DECLARE_END(Role)
-
-
-class LocalFlag {
-private:
-    uint32_t _mask;
-public:
-    LocalFlag() : _mask(0) {}
-    explicit LocalFlag(uint32_t flagNo)
-            : _mask(1UL << flagNo) {
-        assert(flagNo < 32);
-    }
-
-    void init(uint32_t flagNo) {
-        assert(flagNo < 32);
-        _mask = 1UL << flagNo;
-    }
-
-    void set() { IPCLtoRFlagSet(_mask); }
-    void reset() { IPCLtoRFlagClear(_mask); }
-    bool is_set() const { return IPCLtoRFlagBusy(_mask); }
-};
-
-
-class RemoteFlag {
-private:
-    uint32_t _mask;
-public:
-    RemoteFlag() : _mask(0) {}
-    explicit RemoteFlag(uint32_t flagNo)
-            : _mask(1UL << flagNo) {
-        assert(flagNo < 32);
-    }
-
-    void init(uint32_t flagNo) {
-        assert(flagNo < 32);
-        _mask = 1UL << flagNo;
-    }
-
-    void wait() {
-        while(!IPCRtoLFlagBusy(_mask));
-        IPCRtoLFlagAcknowledge(_mask);
-    }
-
-    bool is_set() const { return IPCRtoLFlagBusy(_mask); }
-    void acknowledge() { IPCRtoLFlagAcknowledge(_mask); }
-};
-
-
-class Flag {
-private:
-    Mode _mode;
-public:
-    LocalFlag local;
-    RemoteFlag remote;
-    Flag() {}
-    explicit Flag(uint32_t flagNo, Mode mode)
-            : _mode(mode)
-            , local(flagNo)
-            , remote(flagNo) {}
-
-    void init(uint32_t flagNo, Mode mode) {
-        _mode = mode;
-        local.init(flagNo);
-        remote.init(flagNo);
-    }
-
-    bool is_set() const {
-        switch (_mode.native_value()) {
-        case mcu::c28x::ipc::Mode::singlecore:
-            return local.is_set();
-        case mcu::c28x::ipc::Mode::dualcore:
-            return remote.is_set();
-        }
-        return false;
-    }
-
-    void reset() {
-        switch (_mode.native_value()) {
-        case mcu::c28x::ipc::Mode::singlecore:
-            local.reset();
-            return;
-        case mcu::c28x::ipc::Mode::dualcore:
-            remote.acknowledge();
-            return;
-        }
-    }
-
-    Mode mode() const { return _mode; }
-};
-
-
 SCOPED_ENUM_DECLARE_BEGIN(InterruptType) {
     ipc_interrupt0 = INT_IPC_0,
     ipc_interrupt1 = INT_IPC_1,
@@ -195,29 +86,13 @@ SCOPED_ENUM_DECLARE_BEGIN(InterruptType) {
     ipc_interrupt3 = INT_IPC_3,
 } SCOPED_ENUM_DECLARE_END(InterruptType)
 
-
 inline void registerIpcInterruptHandler(InterruptType ipc_interrupt, void (*handler)(void)) {
     Interrupt_register(ipc_interrupt.underlying_value(), handler);
     Interrupt_enable(ipc_interrupt.underlying_value());
 }
 
-
-namespace flags {
-
-
-//extern mcu::c28x::ipc::Flag cpu1_periphery_configured;
-//extern mcu::c28x::ipc::Flag cpu2_booted;
-//extern mcu::c28x::ipc::Flag cpu2_periphery_configured;
-
-
-} // namespace flags
-
-
 } // namespace ipc
-
 } // namespace c28x
-
 } // namespace mcu
-
 
 #endif
