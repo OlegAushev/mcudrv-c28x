2384544cae9c87e54d496dfff11287a6d40dd054
Oleg Aushev <aushevom@gmail.com>
2025-02-10 12:09:58 +0300

    c28x: format can

--- f2837xd/can/can.cpp
+++ f2837xd/can/can.cpp
@@ -1,74 +1,89 @@
 #ifdef MCUDRV_C28X
 
-
 #include <mcudrv/c28x/f2837xd/can/can.hpp>
-
 
 namespace mcu {
 namespace c28x {
 
 namespace can {
 
-
 const uint32_t impl::can_bases[2] = {CANA_BASE, CANB_BASE};
 const uint32_t impl::can_pie_int_nums[2] = {INT_CANA0, INT_CANB0};
-const SysCtl_CPUSelPeriphInstance impl::can_cpusel_instances[2] = {SYSCTL_CPUSEL_CANA, SYSCTL_CPUSEL_CANB};
+const SysCtl_CPUSelPeriphInstance impl::can_cpusel_instances[2] = {
+        SYSCTL_CPUSEL_CANA,
+        SYSCTL_CPUSEL_CANB};
 
-void (*Module::_on_interrupt_callbacks[peripheral_count])(Module*, uint32_t, uint16_t);
+void (*Module::on_interrupt_callbacks_[peripheral_count])(Module*,
+                                                          uint32_t,
+                                                          uint16_t);
 
-
-Module::Module(Peripheral peripheral, const RxPinConfig& rx_pin, const TxPinConfig& tx_pin, const Config& config)
-        : emb::singleton_array<Module, peripheral_count>(this, peripheral.underlying_value())
-        , _peripheral(peripheral)
-        , _module(impl::can_bases[peripheral.underlying_value()], impl::can_pie_int_nums[peripheral.underlying_value()]) {
+Module::Module(Peripheral peripheral,
+               const RxPinConfig& rx_pin,
+               const TxPinConfig& tx_pin,
+               const Config& config)
+        : emb::singleton_array<Module, peripheral_count>(
+                this, peripheral.underlying_value()),
+          peripheral_(peripheral),
+          module_(impl::can_bases[peripheral.underlying_value()],
+                  impl::can_pie_int_nums[peripheral.underlying_value()]) {
 #ifdef CPU1
-    _init_pins(rx_pin, tx_pin);
+    init_pins(rx_pin, tx_pin);
 #endif
 
-    CAN_initModule(_module.base);
-    CAN_selectClockSource(_module.base, CAN_CLOCK_SOURCE_SYS);
+    CAN_initModule(module_.base);
+    CAN_selectClockSource(module_.base, CAN_CLOCK_SOURCE_SYS);
 
     switch (config.bitrate.native_value()) {
     case Bitrate::bitrate_125k:
     case Bitrate::bitrate_250k:
-        CAN_setBitRate(_module.base, mcu::c28x::sysclk_freq(), static_cast<uint32_t>(config.bitrate.underlying_value()), 16);
+        CAN_setBitRate(module_.base,
+                       mcu::c28x::sysclk_freq(),
+                       static_cast<uint32_t>(config.bitrate.underlying_value()),
+                       16);
         break;
     case Bitrate::bitrate_500k:
     case Bitrate::bitrate_1000k:
-        CAN_setBitRate(_module.base, mcu::c28x::sysclk_freq(), static_cast<uint32_t>(config.bitrate.underlying_value()), 10);
+        CAN_setBitRate(module_.base,
+                       mcu::c28x::sysclk_freq(),
+                       static_cast<uint32_t>(config.bitrate.underlying_value()),
+                       10);
         break;
     }
 
-    CAN_setAutoBusOnTime(_module.base, 0);
-    CAN_enableAutoBusOn(_module.base);
+    CAN_setAutoBusOnTime(module_.base, 0);
+    CAN_enableAutoBusOn(module_.base);
 
     if (config.mode != Mode::normal) {
-        CAN_enableTestMode(_module.base, config.mode.underlying_value());
+        CAN_enableTestMode(module_.base, config.mode.underlying_value());
     }
 
-    CAN_startModule(_module.base);
+    CAN_startModule(module_.base);
 }
 
-
 #ifdef CPU1
-void Module::transfer_control_to_cpu2(Peripheral peripheral, const RxPinConfig& rx_pin, const TxPinConfig& tx_pin) {
-    _init_pins(rx_pin, tx_pin);
+void Module::transfer_control_to_cpu2(Peripheral peripheral,
+                                      const RxPinConfig& rx_pin,
+                                      const TxPinConfig& tx_pin) {
+    init_pins(rx_pin, tx_pin);
     GPIO_setMasterCore(rx_pin.pin, GPIO_CORE_CPU2);
     GPIO_setMasterCore(tx_pin.pin, GPIO_CORE_CPU2);
-    SysCtl_selectCPUForPeripheralInstance(impl::can_cpusel_instances[peripheral.underlying_value()], SYSCTL_CPUSEL_CPU2);
+    SysCtl_selectCPUForPeripheralInstance(
+            impl::can_cpusel_instances[peripheral.underlying_value()],
+            SYSCTL_CPUSEL_CPU2);
 }
 #endif
 
-
 void Module::register_interrupt_handler(void (*handler)(void)) {
-    Interrupt_register(_module.pie_int_num, handler);
-    CAN_enableInterrupt(_module.base, CAN_INT_IE0 | CAN_INT_ERROR | CAN_INT_STATUS);
-    CAN_enableGlobalInterrupt(_module.base, CAN_GLOBAL_INT_CANINT0);
+    Interrupt_register(module_.pie_int_num, handler);
+    CAN_enableInterrupt(module_.base,
+                        CAN_INT_IE0 | CAN_INT_ERROR | CAN_INT_STATUS);
+    CAN_enableGlobalInterrupt(module_.base, CAN_GLOBAL_INT_CANINT0);
 }
 
-
-void Module::register_interrupt_callback(void (*callback)(Module*, uint32_t, uint16_t)) {
-    switch (_peripheral.native_value()) {
+void Module::register_interrupt_callback(void (*callback)(Module*,
+                                                          uint32_t,
+                                                          uint16_t)) {
+    switch (peripheral_.native_value()) {
     case Peripheral::cana:
         register_interrupt_handler(on_interrupt<Peripheral::cana>);
         break;
@@ -76,23 +91,18 @@ void Module::register_interrupt_callback(void (*callback)(Module*, uint32_t, uin
         register_interrupt_handler(on_interrupt<Peripheral::canb>);
         break;
     }
-    _on_interrupt_callbacks[_peripheral.underlying_value()] = callback;
+    on_interrupt_callbacks_[peripheral_.underlying_value()] = callback;
 }
 
-
 #ifdef CPU1
-void Module::_init_pins(const RxPinConfig& rx_pin, const TxPinConfig& tx_pin) {
+void Module::init_pins(const RxPinConfig& rx_pin, const TxPinConfig& tx_pin) {
     GPIO_setPinConfig(rx_pin.mux);
     GPIO_setPinConfig(tx_pin.mux);
 }
 #endif
 
-
 } // namespace can
-
 } // namespace c28x
-
 } // namespace mcu
-
 
 #endif
--- f2837xd/can/can.hpp
+++ f2837xd/can/can.hpp
@@ -1,27 +1,21 @@
 #pragma once
 
-
 #ifdef MCUDRV_C28X
-
 
 #include <emblib/core.hpp>
 #include <mcudrv/c28x/f2837xd/gpio/gpio.hpp>
 #include <mcudrv/c28x/f2837xd/system/system.hpp>
 
-
 namespace mcu {
 namespace c28x {
 namespace can {
-
 
 SCOPED_ENUM_UT_DECLARE_BEGIN(Peripheral, uint32_t) {
     cana,
     canb
 } SCOPED_ENUM_DECLARE_END(Peripheral)
 
-
 const size_t peripheral_count = 2;
-
 
 SCOPED_ENUM_UT_DECLARE_BEGIN(Bitrate, uint32_t) {
     bitrate_125k = 125000,
@@ -30,7 +24,6 @@ SCOPED_ENUM_UT_DECLARE_BEGIN(Bitrate, uint32_t) {
     bitrate_1000k = 1000000,
 } SCOPED_ENUM_DECLARE_END(Bitrate)
 
-
 SCOPED_ENUM_DECLARE_BEGIN(Mode) {
     normal = 0,
     silent = CAN_TEST_SILENT,
@@ -38,7 +31,6 @@ SCOPED_ENUM_DECLARE_BEGIN(Mode) {
     external_loopback = CAN_TEST_EXL,
     silent_loopback = CAN_TEST_SILENT | CAN_TEST_LBACK
 } SCOPED_ENUM_DECLARE_END(Mode)
-
 
 struct MessageObject {
     uint32_t obj_id;
@@ -50,19 +42,15 @@ struct MessageObject {
     uint16_t data_len;
 };
 
-
 struct RxPinConfig { uint32_t pin; uint32_t mux; };
 struct TxPinConfig { uint32_t pin; uint32_t mux; };
-
 
 struct Config {
     Bitrate bitrate;
     Mode mode;
 };
 
-
 namespace impl {
-
 
 struct Module {
     uint32_t base;
@@ -71,56 +59,72 @@ struct Module {
             : base(base_), pie_int_num(pie_int_num_) {}
 };
 
-
 extern const uint32_t can_bases[2];
 extern const uint32_t can_pie_int_nums[2];
 extern const SysCtl_CPUSelPeriphInstance can_cpusel_instances[2];
 
-
 } // namespace impl
 
-
-class Module : public emb::singleton_array<Module, peripheral_count>, private emb::noncopyable {
+class Module : public emb::singleton_array<Module, peripheral_count>,
+               private emb::noncopyable {
 private:
-    const Peripheral _peripheral;
-    impl::Module _module;
+    const Peripheral peripheral_;
+    impl::Module module_;
 public:
-    Module(Peripheral peripheral, const RxPinConfig& rx_pin, const TxPinConfig& tx_pin, const Config& config);
+    Module(Peripheral peripheral,
+           const RxPinConfig& rx_pin,
+           const TxPinConfig& tx_pin,
+           const Config& config);
 #ifdef CPU1
-    static void transfer_control_to_cpu2(Peripheral peripheral, const RxPinConfig& rx_pin, const TxPinConfig& tx_pin);
+    static void transfer_control_to_cpu2(Peripheral peripheral,
+                                         const RxPinConfig& rx_pin,
+                                         const TxPinConfig& tx_pin);
 #endif
-    Peripheral peripheral() const { return _peripheral; }
-    uint32_t base() const { return _module.base; }
+    Peripheral peripheral() const { return peripheral_; }
+    uint32_t base() const { return module_.base; }
 
     bool recv(uint32_t obj_id, uint16_t* data_buf) const {
-        return CAN_readMessage(_module.base, obj_id, data_buf);
+        return CAN_readMessage(module_.base, obj_id, data_buf);
     }
 
     void send(uint32_t obj_id, const uint16_t* data_buf, uint16_t data_len) {
-        CAN_sendMessage(_module.base, obj_id, data_len, data_buf);
+        CAN_sendMessage(module_.base, obj_id, data_len, data_buf);
     }
 
     void setup_message_object(const MessageObject& msg_obj) {
-        CAN_setupMessageObject(_module.base, msg_obj.obj_id, msg_obj.frame_id, msg_obj.frame_type,
-                msg_obj.obj_type, msg_obj.frame_idmask, msg_obj.flags, msg_obj.data_len);
+        CAN_setupMessageObject(module_.base,
+                               msg_obj.obj_id,
+                               msg_obj.frame_id,
+                               msg_obj.frame_type,
+                               msg_obj.obj_type,
+                               msg_obj.frame_idmask,
+                               msg_obj.flags,
+                               msg_obj.data_len);
     }
 
     void register_interrupt_handler(void (*handler)(void));
-    void register_interrupt_callback(void (*callback)(Module*, uint32_t, uint16_t));
-    void enable_interrupts() { Interrupt_enable(_module.pie_int_num); }
-    void disable_interrupts() { Interrupt_disable(_module.pie_int_num); }
-    uint32_t interrupt_cause() const { return CAN_getInterruptCause(_module.base); }
+    void register_interrupt_callback(void (*callback)(Module*,
+                                                      uint32_t,
+                                                      uint16_t));
+    void enable_interrupts() { Interrupt_enable(module_.pie_int_num); }
+    void disable_interrupts() { Interrupt_disable(module_.pie_int_num); }
+    uint32_t interrupt_cause() const {
+        return CAN_getInterruptCause(module_.base);
+    }
 
     void acknowledge_interrupt(uint32_t interrupt_cause) {
-        CAN_clearInterruptStatus(_module.base, interrupt_cause);
-        CAN_clearGlobalInterruptStatus(_module.base, CAN_GLOBAL_INT_CANINT0);
+        CAN_clearInterruptStatus(module_.base, interrupt_cause);
+        CAN_clearGlobalInterruptStatus(module_.base, CAN_GLOBAL_INT_CANINT0);
         Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP9);
     }
 protected:
 #ifdef CPU1
-    static void _init_pins(const RxPinConfig& rx_pin, const TxPinConfig& tx_pin);
+    static void init_pins(const RxPinConfig& rx_pin,
+                          const TxPinConfig& tx_pin);
 #endif
-    static void (*_on_interrupt_callbacks[peripheral_count])(Module*, uint32_t, uint16_t);
+    static void (*on_interrupt_callbacks_[peripheral_count])(Module*,
+                                                             uint32_t,
+                                                             uint16_t);
 
     template <Peripheral::enum_type Periph>
     static interrupt void on_interrupt() {
@@ -128,16 +132,14 @@ protected:
         uint32_t interrupt_cause = CAN_getInterruptCause(module->base());
         uint16_t status = CAN_getStatus(module->base());
 
-        _on_interrupt_callbacks[Periph](module, interrupt_cause, status);
+        on_interrupt_callbacks_[Periph](module, interrupt_cause, status);
 
         module->acknowledge_interrupt(interrupt_cause);
     }
 };
 
-
 } // namespace can
 } // namespace c28x
 } // namespace mcu
-
 
 #endif
