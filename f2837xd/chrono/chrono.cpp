7ddd9d9e1bb4ffdbaf259821bb2cb6ec7029b6ed
Oleg Aushev <aushevom@gmail.com>
2025-02-10 12:49:08 +0300

    c28x: format chrono

--- f2837xd/chrono/chrono.cpp
+++ f2837xd/chrono/chrono.cpp
@@ -1,68 +1,62 @@
 #ifdef MCUDRV_C28X
 
-
 #include <mcudrv/c28x/f2837xd/chrono/chrono.hpp>
 
-
 namespace mcu {
-
 namespace c28x {
-
 namespace chrono {
 
-
-bool steady_clock::_initialized __attribute__((section("shared_mcu_chrono"), retain)) = false;
-volatile int64_t steady_clock::_time __attribute__((section("shared_mcu_chrono"), retain)) = 0;
+bool steady_clock::initialized_ __attribute__((section("shared_mcu_chrono"), retain)) = false;
+volatile int64_t steady_clock::time_ __attribute__((section("shared_mcu_chrono"), retain)) = 0;
 const emb::chrono::milliseconds steady_clock::time_step __attribute__((section("shared_mcu_chrono"), retain)) = emb::chrono::milliseconds(1);
-
 
 #ifdef CPU1
 void steady_clock::init() {
-    _time = 0;
+    time_ = 0;
 
     Interrupt_register(INT_TIMER0, steady_clock::on_interrupt);
 
-    CPUTimer_stopTimer(CPUTIMER0_BASE);             // Make sure timer is stopped
-    CPUTimer_setPeriod(CPUTIMER0_BASE, 0xFFFFFFFF); // Initialize timer period to maximum
-    CPUTimer_setPreScaler(CPUTIMER0_BASE, 0);       // Initialize pre-scale counter to divide by 1 (SYSCLKOUT)
-    CPUTimer_reloadTimerCounter(CPUTIMER0_BASE);    // Reload counter register with period value
+    CPUTimer_stopTimer(CPUTIMER0_BASE);
+    CPUTimer_setPeriod(CPUTIMER0_BASE, 0xFFFFFFFF);
+    CPUTimer_setPreScaler(CPUTIMER0_BASE, 0);
+    CPUTimer_reloadTimerCounter(CPUTIMER0_BASE);
 
-    uint32_t tmp = (uint32_t)((mcu::c28x::sysclk_freq() / 1000) * time_step.count());
-    CPUTimer_setPeriod(CPUTIMER0_BASE, tmp - 1);
-    CPUTimer_setEmulationMode(CPUTIMER0_BASE, CPUTIMER_EMULATIONMODE_STOPAFTERNEXTDECREMENT);
+    const uint32_t sysclk_freq_kHz = mcu::c28x::sysclk_freq() / uint32_t(1000);
+    const uint32_t period = sysclk_freq_kHz * uint32_t(time_step.count()) - 1;
+    CPUTimer_setPeriod(CPUTIMER0_BASE, period);
+    CPUTimer_setEmulationMode(CPUTIMER0_BASE,
+                              CPUTIMER_EMULATIONMODE_STOPAFTERNEXTDECREMENT);
 
     CPUTimer_enableInterrupt(CPUTIMER0_BASE);
     Interrupt_enable(INT_TIMER0);
+
     CPUTimer_startTimer(CPUTIMER0_BASE);
 
-    _initialized = true;
+    initialized_ = true;
 }
 #endif
 
-
-bool high_resolution_clock::_initialized = false;
-uint32_t high_resolution_clock::_period;
-
+bool high_resolution_clock::initialized_ = false;
+uint32_t high_resolution_clock::period_;
 
 void high_resolution_clock::init(emb::chrono::microseconds period) {
-    CPUTimer_stopTimer(CPUTIMER1_BASE);             // Make sure timer is stopped
-    CPUTimer_setPeriod(CPUTIMER1_BASE, 0xFFFFFFFF); // Initialize timer period to maximum
-    CPUTimer_setPreScaler(CPUTIMER1_BASE, 0);       // Initialize pre-scale counter to divide by 1 (SYSCLKOUT)
-    CPUTimer_reloadTimerCounter(CPUTIMER1_BASE);    // Reload counter register with period value
+    CPUTimer_stopTimer(CPUTIMER1_BASE);
+    CPUTimer_setPeriod(CPUTIMER1_BASE, 0xFFFFFFFF);
+    CPUTimer_setPreScaler(CPUTIMER1_BASE, 0);
+    CPUTimer_reloadTimerCounter(CPUTIMER1_BASE);
 
-    _period = (uint32_t)(mcu::c28x::sysclk_freq() / 1000000) * period.count() - 1;
-    CPUTimer_setPeriod(CPUTIMER1_BASE, _period);
-    CPUTimer_setEmulationMode(CPUTIMER1_BASE, CPUTIMER_EMULATIONMODE_STOPAFTERNEXTDECREMENT);
+    const uint32_t sysclk_freq_MHz = mcu::c28x::sysclk_freq() /
+                                     uint32_t(1000000);
+    period_ = sysclk_freq_MHz * uint32_t(period.count()) - 1;
+    CPUTimer_setPeriod(CPUTIMER1_BASE, period_);
+    CPUTimer_setEmulationMode(CPUTIMER1_BASE,
+                              CPUTIMER_EMULATIONMODE_STOPAFTERNEXTDECREMENT);
 
-    _initialized = true;
+    initialized_ = true;
 }
 
-
 } // namespace chrono
-
 } // namespace c28x
-
 } // namespace mcu
-
 
 #endif
--- f2837xd/chrono/chrono.hpp
+++ f2837xd/chrono/chrono.hpp
@@ -1,84 +1,84 @@
 #pragma once
 
-
 #ifdef MCUDRV_C28X
-
 
 #include <mcudrv/c28x/f2837xd/system/system.hpp>
 #include <emblib/core.hpp>
 #include <emblib/chrono.hpp>
 #include <emblib/static_vector.hpp>
 
-
 namespace mcu {
-
 namespace c28x {
-
 namespace chrono {
 
-
 inline void delay(emb::chrono::nanoseconds ns) {
-    const uint32_t sysclkCycle_ns = 1000000000 / DEVICE_SYSCLK_FREQ;
-    const uint32_t delayLoop_ns = 5 * sysclkCycle_ns;
-    const uint32_t delayOverhead_ns = 9 * sysclkCycle_ns;
+    const uint32_t sysclk_cycle_ns = 1000000000 / DEVICE_SYSCLK_FREQ;
+    const uint32_t delay_loop_ns = 5 * sysclk_cycle_ns;
+    const uint32_t delay_overhead_ns = 9 * sysclk_cycle_ns;
 
-    if (ns.count() < delayLoop_ns + delayOverhead_ns) {
+    if (ns.count() < delay_loop_ns + delay_overhead_ns) {
         SysCtl_delay(1);
     } else {
-        SysCtl_delay((ns.count() - delayOverhead_ns) / delayLoop_ns);
+        SysCtl_delay((ns.count() - delay_overhead_ns) / delay_loop_ns);
     }
 }
-
 
 inline void delay(emb::chrono::microseconds us) {
     DEVICE_DELAY_US(us.count());
 }
 
-
 inline void delay(emb::chrono::milliseconds ms) {
     delay(emb::chrono::duration_cast<emb::chrono::microseconds>(ms));
 }
 
-
 class steady_clock {
 private:
-    static bool _initialized;
-    static volatile int64_t _time;
+    static bool initialized_;
+    static volatile int64_t time_;
     static const emb::chrono::milliseconds time_step;
 private:
-    steady_clock();                                     // no constructor
-    steady_clock(const steady_clock& other);            // no copy constructor
-    steady_clock& operator=(const steady_clock& other); // no copy assignment operator
+    steady_clock();                                     // delete
+    steady_clock(const steady_clock& other);            // delete
+    steady_clock& operator=(const steady_clock& other); // delete
 public:
-    static bool initialized() { return _initialized; }
-    static emb::chrono::milliseconds now() { return emb::chrono::milliseconds(_time); }
+    static bool initialized() { return initialized_; }
+
+    static emb::chrono::milliseconds now() {
+        return emb::chrono::milliseconds(time_);
+    }
+
     static emb::chrono::milliseconds step() { return time_step; }
 #ifdef CPU1
     static void init();
-    static void reset() { _time = 0; }
+    static void reset() { time_ = 0; }
 protected:
     static interrupt void on_interrupt() {
-        _time += time_step.count();
+        time_ += time_step.count();
         Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1);
     }
 #endif
 };
 
-
 class high_resolution_clock {
 private:
-    static bool _initialized;
-    static uint32_t _period;
+    static bool initialized_;
+    static uint32_t period_;
     static const int64_t sysclk_period_ns = 1000000000 / DEVICE_SYSCLK_FREQ;
 private:
-    high_resolution_clock();                                                // no constructor
-    high_resolution_clock(const high_resolution_clock& other);              // no copy constructor
-    high_resolution_clock& operator=(const high_resolution_clock& other);   // no copy assignment operator
+    high_resolution_clock();                                    // delete
+    high_resolution_clock(const high_resolution_clock& other);  // delete
+    high_resolution_clock& operator=(const high_resolution_clock& other);
 public:
     static void init(emb::chrono::microseconds period);
-    static bool initialized() { return _initialized; }
+    static bool initialized() { return initialized_; }
+
     static uint32_t counter() { return CPUTimer_getTimerCount(CPUTIMER1_BASE); }
-    static emb::chrono::nanoseconds now() {	return emb::chrono::nanoseconds((_period - counter()) * sysclk_period_ns); }
+
+    static emb::chrono::nanoseconds now() {
+        return emb::chrono::nanoseconds((period_ - counter()) *
+                                        sysclk_period_ns);
+    }
+
     static void start() { CPUTimer_startTimer(CPUTIMER1_BASE); }
     static void stop() { CPUTimer_stopTimer(CPUTIMER1_BASE); }
 
@@ -91,12 +91,8 @@ private:
     static interrupt void on_interrupt();
 };
 
-
 } // namespace chrono
-
 } // namespace c28x
-
 } // namespace mcu
-
 
 #endif
